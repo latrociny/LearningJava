4d0081d235dac163c54372e6ac1f50e4a9164a72
Bhatt <bhattk2@delagelanden.com>
2019-01-23 14:07:03 +0100

    made bounds on increment and decrement buttons, localization + intents TODO

--- JustJava2/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava2/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,6 +10,8 @@ package com.example.android.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,7 +26,11 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    //lower bound/default value of coffee is always 2
+    int quantity = 2;
+    boolean whippedCreamQ;
+    boolean chocolateQ;
+    private String name;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -33,11 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
+    /**order_summary_text_view
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceText = createOrderSummary(quantity);
+        String priceText = createOrderSummary(quantity, whippedCreamQ, chocolateQ);
         displayMessage(priceText);
     }
 
@@ -47,40 +53,73 @@ public class MainActivity extends AppCompatActivity {
      * @return whole shebang
      *
      */
-    private String createOrderSummary (int quantity) {
-        int price = quantity * 5;
-        String naam = "Name: Keshav";
-        return naam + "\n" + "Quantity: " + quantity + "\n" + "Total: $ " + price + "\nThank you!";
+    private String createOrderSummary (int quantity, boolean whippedCreamQ, boolean chocolateQ) {
+        int pricePerCoffee = 5;
+        int pricePerWhippedCream = 1;
+        int pricePerChocolate = 2;
+        int whippedCreamNr = whippedCreamQ ? 1 : 0;
+        int chocolateNr = chocolateQ ? 1 : 0;
+        int totalPrice = quantity * (pricePerCoffee + (pricePerWhippedCream * whippedCreamNr)
+                + (pricePerChocolate * chocolateNr));
+        return name + "\n" + "Add whipped cream? " + whippedCreamQ +  "\n" +
+                 "Add chocolate? " + chocolateQ +  "\n" +
+                "Quantity: " + quantity + "\n" + "Total: $ " + totalPrice + "\nThank you!";
+    }
+
+    /**
+     * This method sets the message from the submitOrder method on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        if (quantity >= 100) {
+            Toast.makeText(getApplicationContext(),"Cannot order more than " +
+                    "100 coffees",Toast.LENGTH_SHORT).show();
+        } else {
+            quantity = quantity + 1;
+            displayQuantity(quantity);
+        }
     }
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
+        if (quantity <= 1) {
+            Toast.makeText(getApplicationContext(),"Cannot order less than " +
+                    "1 coffee",Toast.LENGTH_SHORT).show();
+//            Equivalent is to use this instead of getApplicationContext
+//             Toast.makeText(this,"Cannot order less than " +
+//                    "1 coffee",Toast.LENGTH_SHORT).show();
+        } else {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
+        }
     }
 
-    //To show if the check box was checked on not
-    //Example:
-//        boolean mybool;
-//        if (faculty.isChecked()) {
-//            mybool = true;
-//
-//        } else {
-//            mybool = false;
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
 
-    public void indicateBoxChecked(View v) {
+    public void userInputs (View v) {
         CheckBox whippedCreamCheckBox = findViewById(R.id.whippedCreamCheckBox);
-        boolean checked = whippedCreamCheckBox.isChecked();
-        Log.v(TAG, "Checkbox value is " + checked );
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolateCheckBox);
+        TextView nameView = findViewById(R.id.name);
+
+        name = nameView.getText().toString();
+        whippedCreamQ = whippedCreamCheckBox.isChecked();
+        chocolateQ = chocolateCheckBox.isChecked();
+
+        //Log.v(TAG, "Checkbox value is " + checked );
         }
 
     /** This is another way of implementing the above
@@ -92,21 +131,8 @@ public class MainActivity extends AppCompatActivity {
     }
 */
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
 
-    /**
-     * This method sets the message from the submitOrder method on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
 
     // ****These are not used anymore*********
 
@@ -127,3 +153,12 @@ public class MainActivity extends AppCompatActivity {
     }
      */
 }
+
+//To show if the check box was checked on not
+//Example:
+//        boolean mybool;
+//        if (faculty.isChecked()) {
+//            mybool = true;
+//
+//        } else {
+//            mybool = false;
